e213484102cb11459126c3b502cd160b24fcc773
htsiang <htsiang3@gmail.com>
2019-08-10 10:52:25 -0700

    Create mongoose schemas for exercise & user documents. Create seed script for exercise collection.

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,2 +1,26 @@
-var fs = require('fs');
-var mongoose = require('mongoose');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Models = require('./dbModels');
+let exerciseData; // for storing data.txt into array
+
+mongoose.connect('mongodb://localhost:27017/swolDB', {useNewUrlParser});
+
+fs.readFile('data.txt', 'utf8', (err, data) => {
+    if(err) throw err;
+
+    // turn data.txt into an array
+    exerciseData = data.split('\n').join(',').split(',');
+    console.log(exerciseData);
+
+    for(let i=0; i<exerciseData.length; i+=2) {
+        // place array data into object for insertion into database
+        let data = {
+            name: exerciseData[i],
+            muscle: exerciseData[i+1]
+        };
+
+        Models.Exercise.create(data)
+            .then(dbExample => console.log(dbExample))
+            .catch(err => console.log(err.message));
+    };
+});
